314c50ea589bfd5dbb565df783bf3ed20bed26d4
premprakashgupta <prem.com0011@gmail.com>
2024-02-13 12:08:43 +0530

    arrange the directory

---
+++ backend/sunshine/src/universalLink.js
@@ -0,0 +1,4 @@
+const univesalLink="https://sunshine-server.onrender.com/api/ppg";
+// const univesalLink = "http://localhost:4000/api/ppg";
+
+export default univesalLink;
--- sunshine/src/universalLink.js
+++
@@ -1,4 +0,0 @@
-// const univesalLink="https://sunshineprem.herokuapp.com/api/ppg";
-const univesalLink = "http://localhost:4000/api/ppg";
-
-export default univesalLink;
